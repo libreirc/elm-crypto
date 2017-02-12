615a8d20bd488a74b7960a9d703f7c99795e205a
Hyeon Kim <simnalamburt@gmail.com>
2017-02-12 23:45:20 +0900

    Update native code for Elm 0.17

--- src/Native/SecureRandom.js
+++ src/Native/SecureRandom.js
@@ -1,63 +1,72 @@
-Elm.Native.SecureRandom = {}; // eslint-disable-line no-undef
-Elm.Native.SecureRandom.make = function(localRuntime) {  // eslint-disable-line no-undef
-  localRuntime.Native = localRuntime.Native || {};
-  localRuntime.Native.SecureRandom = localRuntime.Native.SecureRandom || {};
-  if (localRuntime.Native.SecureRandom.values) {
-    return localRuntime.Native.SecureRandom.values;
+var _openirc$elm_random_secure$Native_SecureRandom = function() {
+'use strict';
+
+
+//
+// Check `crypto.getRandomValues()` compatibility
+//
+function fallback() {
+  function fail(_parameter) {
+    return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
+      callback(
+        _elm_lang$core$Native_Scheduler.fail(
+          _openirc$elm_random_secure$Random_Secure$NoGetRandomValues
+        )
+      );
+    });
   }
 
-  var Task = Elm.Native.Task.make(localRuntime);  // eslint-disable-line no-undef
+  return {
+    getRandomInts: fail,
+    getRandomInt: fail
+  };
+}
 
-  if (typeof window === 'object' && typeof window.crypto === 'object'
-    && typeof window.crypto.getRandomValues === 'function') {
-    var crypto = window.crypto;
+// TODO: Test it manually with IE 10
+if (typeof window !== 'object') { return fallback(); }
+var crypto = window.crypto || window.msCrypto;
+if (typeof crypto !== 'object' || typeof crypto.getRandomValues !== 'function') { return fallback(); }
 
-    function runAsTask(action) {
-      return Task.asyncFunction(function (callback) {
-        var result, errorName = null, errorMessage = null;
 
-        try { result = action(); }
-        catch (err) {
-          if (typeof err === 'object') {
-            errorName = err.name || "";
-            errorMessage = err.message || err.toString();
-          } else {
-            errorName = typeof err;
-            errorMessage = err.toString();
-          }
-        }
+//
+// Implementation
+//
+function makeFn(retrieve) {
+  return function(count) {
+    return _elm_lang$core$Native_Scheduler.nativeBinding(function(callback) {
+      var values;
 
-        if (errorName !== null) {
-          callback(Task.fail({ ctor: 'Exception', _0: errorName, _1: errorMessage }));
+      try {
+        values = retrieve();
+      } catch (err) {
+        // An exception has been thrown
+        var errorName, errorMessage;
+        if (typeof err === 'object') {
+          errorName = err.name || "";
+          errorMessage = err.message || err.toString();
         } else {
-          callback(Task.succeed(result));
+          errorName = typeof err;
+          errorMessage = err.toString();
         }
-      });
-    }
 
-    return localRuntime.Native.SecureRandom.values = {
-      getRandomInts: function (count) {
-        return runAsTask(function () {
-          return crypto.getRandomValues(new Uint32Array(count));
-        });
-      },
-
-      getRandomInt: function () {
-        return runAsTask(function () {
-          return crypto.getRandomValues(new Uint32Array(1))[0];
-        });
+        callback(
+          _elm_lang$core$Native_Scheduler.fail(
+            A2(_openirc$elm_random_secure$Random_Secure$Exception, errorName, errorMessage)
+          )
+        );
+        return;
       }
-    };
-  } else {
-    var fail = function () {
-      return Task.asyncFunction(function (callback) {
-        callback(Task.fail({ ctor: 'NoGetRandomValues' }));
-      });
-    };
 
-    return localRuntime.Native.SecureRandom.values = {
-      getRandomInts: fail,
-      getRandomInt: fail
-    };
-  }
+      callback(_elm_lang$core$Native_Scheduler.succeed(values));
+    });
+  };
+}
+
+var crypto = window.crypto || window.msCrypto;
+return {
+  getRandomInts: makeFn(function() { return crypto.getRandomValues(new Uint32Array(count)); }),
+  getRandomInt: makeFn(function() { return crypto.getRandomValues(new Uint32Array(1))[0]; })
 };
+
+
+}();
