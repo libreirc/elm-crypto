1b53406666bd79f7531cbd7240a20e53a1d719ae
Hyeon Kim <simnalamburt@gmail.com>
2017-02-13 02:04:58 +0900

    Handle the error properly in sample app

--- example/CoinFlip.elm
+++ example/CoinFlip.elm
@@ -6,42 +6,57 @@ import Html.App as App
 import Html exposing (..)
 import Html.Events exposing (..)
 
-import Random.Secure
+import Random.Secure as Crypto
 
-type alias Model = { coin : Maybe Bool }
+type alias Model =
+  {
+    coin : Maybe Bool,
+    logs : List String
+  }
 
-type Msg = Toss | Catch Bool | Failed
+type Msg = Toss | Catch Bool | Failed Crypto.Error
 
 init : (Model, Cmd Msg)
-init = ({ coin = Nothing }, Cmd.none)
+init = ({ coin = Nothing, logs = [] }, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update action model =
   case action of
     Toss ->
       let
-        errorToMsg = \_ -> Failed
-        contentToMsg = \value -> Catch value
-        cmd = Task.perform errorToMsg contentToMsg Random.Secure.bool
+        cmd = Task.perform Failed Catch Crypto.bool
       in (model, cmd)
     Catch coin -> ({ model | coin = Just coin }, Cmd.none)
-    Failed ->
-      -- TODO: Error Handling
-      (model, Cmd.none)
+    Failed error ->
+      let
+        log = case error of
+          Crypto.NoGetRandomValues -> "There's no getRandomValues() function."
+          Crypto.Exception _ _ -> "An exception has been occurred" -- TODO
+        newLogs = log::(model.logs)
+      in
+      ({ model | logs = newLogs }, Cmd.none)
 
 view : Model -> Html Msg
 view model =
-  div [] <|
-    case model.coin of
-      Nothing ->
-        [
-          button [ onClick Toss ] [ text "Toss a coin..." ]
-        ]
-      Just coin ->
-        [
-          p [] [ text <| if coin then "Heads!" else "Tails!" ],
-          button [ onClick Toss] [ text "Toss again!" ]
-        ]
+  let
+    logs = List.map (\log -> li [] [text log]) model.logs
+  in
+    section [] [
+      div [] (
+        case model.coin of
+          Nothing ->
+            [
+              button [ onClick Toss ] [ text "Toss a coin..." ]
+            ]
+          Just coin ->
+            [
+              button [ onClick Toss] [ text "Toss again!" ],
+              p [] [ text <| if coin then "Heads!" else "Tails!" ]
+            ]
+      ),
+      hr [] [],
+      ul [] logs
+    ]
 
 main = App.program
   {
